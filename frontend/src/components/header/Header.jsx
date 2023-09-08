57ed40ead72a0b77a14424a01646447bcdaf9efe
Obinna Uko <obicodeman@yahoo.com>
2023-09-08 11:41:28 +0100

    app header design for all screen devices

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,36 +1,51 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { Icon } from "@iconify/react"
 import UserPicker from "../user-picker/UserPicker";
+import MobileSideNav from "../mobile-side-nav/MobileSideNav";
+import "./header.scss";
 
 const Header = () => {
+    const [showMobileSideNav, setShowSideNav] = useState(false)
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
+        <header className="header">
+            <div className="container header-content">
+                <nav className="header-navs">
+                    <ul className="header-navs--list">
+                        <li>
 
-                        <Link to="/bookings">
-                            <Icon icon="ion:calendar-outline" />
-                            <span>Bookings</span>
-                        </Link>
-                    </li>
-                    <li>
+                            <NavLink to="/bookings" className={({ isActive }) => isActive ? "active" : ""}>
+                                <Icon icon="ion:calendar-outline" className="small-icon" />
+                                <span>Bookings</span>
+                            </NavLink>
+                        </li>
+                        <li>
 
-                        <Link to="/bookables">
-                            <Icon icon="lucide:door-open" />
-                            <span>Bookables</span>
-                        </Link>
-                    </li>
-                    <li>
+                            <NavLink to="/bookables" className={({ isActive }) => isActive ? "active" : ""}>
+                                <Icon icon="lucide:door-open" className="small-icon" />
+                                <span>Bookables</span>
+                            </NavLink>
+                        </li>
+                        <li>
 
-                        <Link to="/users">
-                            <Icon icon="clarity:users-solid" />
-                            <span>Users</span>
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <UserPicker />
+                            <NavLink to="/users" className={({ isActive }) => isActive ? "active" : ""}>
+                                <Icon icon="clarity:users-solid" className="small-icon" />
+                                <span>Users</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="header-right">
+                    <UserPicker />
+                    <button type="button" className="mobile-nav--toggler show-on-mobile" onClick={() => setShowSideNav(prev => !prev)}>
+                        <Icon icon="mdi:hamburger-menu-back" className="medium-icon" />
+                    </button>
+                    {
+                        showMobileSideNav ? <MobileSideNav /> : null
+                    }
+                </div>
+            </div>
+
         </header>
     )
 }
